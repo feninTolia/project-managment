25a567ff450914d9da76ec59380e2c91e5955c79
Anatolii Fenin <fenintolja@gmail.com>
2024-10-24 15:35:09 +0300

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ app.use('/users', userRoutes);
 app.use('/teams', teamRoutes);
 
 // SERVER
-const PORT = process.env.PORT ?? 8001;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 8001;
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Server is running on port - ' + PORT);
 });
