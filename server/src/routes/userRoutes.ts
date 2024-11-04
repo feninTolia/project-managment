e2502a299596d247f0886160bd3a0197131b38c7
Anatolii Fenin <fenintolja@gmail.com>
2024-11-04 12:34:09 +0200

    add create user route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { getUsers } from '../controllers/userController';
+import { getUsers, postUser } from '../controllers/userController';
 
 const router = Router();
 router.get('/', getUsers);
+router.post('/create-user', postUser);
 
 export default router;
